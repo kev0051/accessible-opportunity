06a7e93d46f5f625f45c3eec6d7004c0f9f35dee
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-03-01 17:37:12 -0600

    Initial django project
    Set up admin, user registration/login, password hashing, cfa authentication, and invalid login notif

---
+++ src/personal/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+#from personal.models import Question
+
+# Register your models here.
+
+# new table created to display in admin console
+#admin.site.register(Question)
---
+++ src/personal/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from account.models import Account
+#from personal.models import Question
+
+# Create your views here.
+
+def home_screen_view(request):
+	
+	context = {}
+
+	accounts = Account.objects.all()
+	context['accounts'] = accounts
+	#context['some_string'] = "this is some string that I'm passing to the view"
+	#context['some_number'] = 6300
+
+	#list_of_values = []
+	#list_of_values.append("first entry")
+	#list_of_values.append("second entry")
+	#list_of_values.append("third entry")
+	#list_of_values.append("fourth entry")
+	#context['list_of_values'] = list_of_values
+
+	#questions = Question.objects.all()
+	#context['questions'] = questions
+
+	return render(request, "personal/home.html", context)
