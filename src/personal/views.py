766d243702ac4293f874d43c4be423c5dcd9b7c0
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-03-26 11:19:18 -0500

    troubleshooted about page with Gaige

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from account.models import Account
-#from personal.models import Question
 
 # Create your views here.
 
@@ -10,17 +9,11 @@ def home_screen_view(request):
 
 	accounts = Account.objects.all()
 	context['accounts'] = accounts
-	#context['some_string'] = "this is some string that I'm passing to the view"
-	#context['some_number'] = 6300
 
-	#list_of_values = []
-	#list_of_values.append("first entry")
-	#list_of_values.append("second entry")
-	#list_of_values.append("third entry")
-	#list_of_values.append("fourth entry")
-	#context['list_of_values'] = list_of_values
-
-	#questions = Question.objects.all()
-	#context['questions'] = questions
 
 	return render(request, "personal/home.html", context)
+
+def about_screen_view(request):
+    context = {}
+    return render(request, "personal/about.html", context)
+	
