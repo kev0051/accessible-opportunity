632f59066a8c8a5e1a70ca56b11d02ae6711f7d8
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-03-27 21:38:56 -0500

    Add files via upload

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
+from django.http import HttpResponse
 
 from blog.models import BlogPost
-from blog.forms import CreateBlogPostForm
+from blog.forms import CreateBlogPostForm, UpdateBlogPostForm
 from account.models import Account
+
 
 def create_blog_view(request):
 
@@ -12,16 +15,18 @@ def create_blog_view(request):
 	if not user.is_authenticated:
 		return redirect('must_authenticate')
 
-	form=CreateBlogPostForm(request.POST or None, request.FILES or None)
+	form = CreateBlogPostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		obj = form.save(commit=False)
 		author = Account.objects.filter(email=user.email).first()
 		obj.author = author
 		obj.save()
-		form=CreateBlogPostForm()
+		form = CreateBlogPostForm()
 
+	context['form'] = form
 
 	return render(request, "blog/create_blog.html", context)
+
 
 def detail_blog_view(request, slug):
 
@@ -31,3 +36,38 @@ def detail_blog_view(request, slug):
 	context['blog_post'] = blog_post
 
 	return render(request, 'blog/detail_blog.html', context)
+
+
+
+def edit_blog_view(request, slug):
+
+	context = {}
+
+	user = request.user
+	if not user.is_authenticated:
+		return redirect("must_authenticate")
+
+	blog_post = get_object_or_404(BlogPost, slug=slug)
+
+	if blog_post.author != user:
+		return HttpResponse('You are not the author of that post.')
+
+	if request.POST:
+		form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+		if form.is_valid():
+			obj = form.save(commit=False)
+			obj.save()
+			context['success_message'] = "Updated"
+			blog_post = obj
+
+	form = UpdateBlogPostForm(
+			initial = {
+					"title": blog_post.title,
+					"body": blog_post.body,
+					"image": blog_post.image,
+			}
+		)
+
+	context['form'] = form
+	return render(request, 'blog/edit_blog.html', context)
+
