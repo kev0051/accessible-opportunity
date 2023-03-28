6fc682698a69f4da36fa5c9c1cb33a1f108157d8
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-03-27 23:20:26 -0500

    blog feed

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from account.models import Account
+from operator import attrgetter
+from blog.models import BlogPost
 #from personal.models import Question
 
 # Create your views here.
@@ -8,8 +10,10 @@ def home_screen_view(request):
 	
 	context = {}
 
+	blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
 	accounts = Account.objects.all()
 	context['accounts'] = accounts
+	context['blog_posts'] = blog_posts
 
 	user = request.user
 	if user.is_authenticated:
