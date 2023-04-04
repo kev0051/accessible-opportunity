f3b898281eb5bd4140edc8ff70c86a3e0897fb41
kev0051 <126007369+kev0051@users.noreply.github.com>
2023-04-04 18:28:09 -0500

    Update models.py
    Added disability tag field

--- src/blog/models.py
+++ src/blog/models.py
@@ -14,13 +14,25 @@ def upload_location(instance, filename, **kwargs):
 		)
 	return file_path
 
+# REFERENCE: https://docs.djangoproject.com/en/4.1/topics/db/models/
+
 class BlogPost(models.Model):
+	is_active			= models.BooleanField(default=True) # If true, can be discovered. Posters can set this to false after they create it.
 	title				= models.CharField(max_length=50, null=False, blank=False)
 	body				= models.CharField(max_length=20000, null=False, blank=False)
 	image				= models.ImageField(upload_to=upload_location, null=False, blank=False)
 	date_published		= models.DateTimeField(auto_now_add=True, verbose_name="date published")
 	date_updated		= models.DateTimeField(auto_now=True, verbose_name="date updated")
 	author				= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	#date_expired		= models.DateTimeField(auto_now_add=True, verbose_name="date expired") # (SET BY POSTER AT CREATION) If past this date, is_active is set to false
+	DIS_TYPE			= (
+							('V', 'Visual'),
+							('A', 'Auditory'),
+							('P', 'Physical'),
+							('I', 'Intellectual'),
+							('O', 'Other')
+						  )
+	disability			= models.CharField(default='O', max_length=1, choices=DIS_TYPE)
 	slug				= models.SlugField(blank=True, unique=True)
 
 	def __str__(self):
